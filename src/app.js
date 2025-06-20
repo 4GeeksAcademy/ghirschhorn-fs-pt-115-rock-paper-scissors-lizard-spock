cd8d4a49958948a0c38e706941f11bfe26250b8d
Gerardo Hirschhorn <ghirschhorn@outlook.com>
2025-06-20 10:39:09 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,3 +1,34 @@
 //write your code here
 console.log("Hello Rigo from the console!");
 
+const weapon = ["rock", "paper", "scissors", "lizard", "spock"]
+
+const getRandomWeapon = () => {
+const index = Math.floor(Math.random() * weapon.length)
+return weapon[index]
+}
+
+const checkWeaponGuess = (userGuest) => {
+    const computerWeapon = getRandomWeapon()
+
+    console.log(`You chose: ${userGuest}`);
+	console.log(`Computer weapon: ${computerWeapon}`);
+
+    if (userGuest === computerWeapon) {
+        console.log("Take it serius! It`s a draw...");
+    } else if (
+        (userGuest === "rock" && (computerWeapon === "scissors" || "lizard")) ||
+        (userGuest === "paper" && (computerWeapon === "rock" || "spock")) ||
+        (userGuest === "scissors" && (computerWeapon === "paper" || "lizard")) ||
+        (userGuest === "lizard" && (computerWeapon === "paper" || "spock")) ||
+        (userGuest === "spock" && (computerWeapon === "scissors" || "rock"))
+    ) {
+        console.log("Hmm... you win. Again!");
+    } else {
+        console.log("Bazinga! You loose!");
+    }
+}
+
+const guess = prompt("Choose your weapon: rock, paper, scissors, lizard, spock").toLowerCase();
+
+checkWeaponGuess(guess)
