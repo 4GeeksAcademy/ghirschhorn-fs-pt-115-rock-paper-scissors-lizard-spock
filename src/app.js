4a8befb7c223e42a5a2ef5a22197215a9f70ee00
Gerardo Hirschhorn <ghirschhorn@outlook.com>
2025-06-25 10:01:00 +0000

    corregido

--- src/app.js
+++ src/app.js
@@ -3,32 +3,51 @@ console.log("Hello Rigo from the console!");
 
 const weapon = ["rock", "paper", "scissors", "lizard", "spock"]
 
-const getRandomWeapon = () => {
-const index = Math.floor(Math.random() * weapon.length)
-return weapon[index]
+const gameOptions = {
+    rock : ["scissors", "lizard"],
+    paper : ["rock", "spock"],
+    scissors : ["paper", "lizard"],
+    lizard : ["paper", "spock"],
+    spock : ["scissors", "rock"],
 }
 
-const checkWeaponGuess = (userGuest) => {
-    const computerWeapon = getRandomWeapon()
-
-    console.log(`You chose: ${userGuest}`);
-	console.log(`Computer weapon: ${computerWeapon}`);
-
-    if (userGuest === computerWeapon) {
-        console.log("Take it serius! It`s a draw...");
-    } else if (
-        (userGuest === "rock" && (computerWeapon === "scissors" || "lizard")) ||
-        (userGuest === "paper" && (computerWeapon === "rock" || "spock")) ||
-        (userGuest === "scissors" && (computerWeapon === "paper" || "lizard")) ||
-        (userGuest === "lizard" && (computerWeapon === "paper" || "spock")) ||
-        (userGuest === "spock" && (computerWeapon === "scissors" || "rock"))
-    ) {
-        console.log("Hmm... you win. Again!");
-    } else {
-        console.log("Bazinga! You loose!");
-    }
+function Play(yourChoise) {
+    yourChoise.toLowerCase();
+    const computerChoise = weapon [Math.floor(Math.random() * weapon.length)];
+    console.log(computerChoise);
+    
+    if (yourChoise === computerChoise) console.log("Take it serius! It`s a draw...");
+    else if (gameOptions[yourChoise].includes(computerChoise)) console.log("Hmm... you win. Again!");
+    else console.log("Bazinga! You loose!");
 }
 
 const guess = prompt("Choose your weapon: rock, paper, scissors, lizard, spock").toLowerCase();
 
-checkWeaponGuess(guess)
+Play(guess)
+
+
+// const getRandomWeapon = () => {
+// const index = Math.floor(Math.random() * weapon.length)
+// return weapon[index]
+// }
+
+// const checkWeaponGuess = (userGuest) => {
+//     const computerWeapon = getRandomWeapon()
+
+//     console.log(`You chose: ${userGuest}`);
+// 	console.log(`Computer weapon: ${computerWeapon}`);
+
+//     if (userGuest === computerWeapon) console.log("Take it serius! It`s a draw...");
+//     else if (
+//         (userGuest === "rock" && (computerWeapon === "scissors" || "lizard")) ||
+//         (userGuest === "paper" && (computerWeapon === "rock" || "spock")) ||
+//         (userGuest === "scissors" && (computerWeapon === "paper" || "lizard")) ||
+//         (userGuest === "lizard" && (computerWeapon === "paper" || "spock")) ||
+//         (userGuest === "spock" && (computerWeapon === "scissors" || "rock"))
+//     ) console.log("Hmm... you win. Again!");
+//     else console.log("Bazinga! You loose!");
+// }
+
+// const guess = prompt("Choose your weapon: rock, paper, scissors, lizard, spock").toLowerCase();
+
+// checkWeaponGuess(guess)
